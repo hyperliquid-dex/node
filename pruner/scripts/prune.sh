83b9da25884447408233d82f17fc18528c70b94e
AutoStake <22457974+AutoStake-com@users.noreply.github.com>
2024-07-21 07:46:44 +0100

    decrease pruner to prune data older than 2 days old

--- pruner/scripts/prune.sh
+++ pruner/scripts/prune.sh
@@ -1,4 +1,5 @@
 DATA_PATH="/home/hluser/hl/data"
 
-# Delete data older than 1 days.
-find "$DATA_PATH" -mindepth 1 -mtime +1 -depth -delete
+# Delete data older than 2 days.
+find "$DATA_PATH" -mindepth 1 -mtime +2 -depth -delete
+
