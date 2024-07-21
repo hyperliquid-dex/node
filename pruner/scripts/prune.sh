f08b02cfc92a0bd1eaf44bc7416d8dd0905eca32
AutoStake <22457974+AutoStake-com@users.noreply.github.com>
2024-07-21 07:55:41 +0100

    fix global positional arguments

--- pruner/scripts/prune.sh
+++ pruner/scripts/prune.sh
@@ -1,5 +1,5 @@
 DATA_PATH="/home/hluser/hl/data"
 
 # Delete data older than 2 days.
-find "$DATA_PATH" -mindepth 1 -mtime +2 -depth -delete
+find "$DATA_PATH" -mindepth 1 -depth -mtime +2 -delete
 
